312ae25dc39b3c76513ac35a8d60bc73ec8ba253
Madison Kalivoda <mkalivoda07@gmail.com>
2018-03-03 13:47:33 -0700

    initial upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
 
 
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare let $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
-  constructor() { }
-
+export class HomeComponent implements OnInit, AfterViewInit {
+  ngOnInit() {
+    $('.carousel').carousel();
+    setTimeout($('.carousel').carousel('next'), 4000);
+  }
+  ngAfterViewInit() {
+    $('.carousel').carousel();
+    setInterval(() => { $('.carousel').carousel('next') }, 4000);
+  }
+  constructor(private router: Router) { }
+  routeSignUp() {
+    this.router.navigate(["signup"]);
+  }
+  routeLogIn() {
+    this.router.navigate(["login"]);
+  }
+  autoplay() {
+    $('.carousel').carousel('next'), 4000);
+  }
 }
